6cbc2418cd5e6b3b67b1ba9cfd9cd0acba2bf18d
baguettecroissant <wade.perso@gmail.com>
2026-02-07 13:23:58 +0400

    feat: setup Umami analytics and remove GA

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -3,27 +3,14 @@
 import Script from "next/script";
 
 export default function Analytics() {
-    const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
-
-    if (!GA_ID) return null;
-
     return (
         <>
+            {/* Umami Analytics */}
             <Script
+                defer
+                src="https://cloud.umami.is/script.js"
+                data-website-id="451a5087-9fa8-4adc-8f42-2b3c04000c12"
                 strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-            />
-            <Script
-                id="google-analytics"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_ID}');
-          `,
-                }}
             />
         </>
     );
